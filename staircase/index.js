82e0cdf347220842c2cdd256f30d33595aa70de0
jlmack13 <jennimacklin@gmail.com>
2018-10-10 10:14:20 -0400

    added first basic test and solution.

---
+++ staircase/index.js
@@ -0,0 +1,30 @@
+/* Staircase problem with right aligned staircase.
+
+Steps go up from left to right.
+
+Take an input of a num n and print a staircase n levels deep, 
+using spaces where there are no stairs and # for stairs.
+
+Ex: n = 4
+"   #"
+"  ##"
+" ###"
+"####"
+
+Problem from HackerRank: https: //www.hackerrank.com/challenges/staircase/
+
+*/
+
+function staircase(n) {
+    for (let row = 0; row < n; row++) {
+        let stair = "";
+        for (let col = 0; col < n; col++) {
+            if ((col - (n - 1) + row) < 0) {
+                stair += " ";
+            } else {
+                stair += "#"
+            }
+        }
+        console.log(stair)
+    }
+}
---
+++ staircase/test.js
@@ -0,0 +1,5 @@
+const staircase = require('./index.js');
+
+test('staircase function exists', () => {
+    expect(typeof staircase).toEqual('function')
+});
